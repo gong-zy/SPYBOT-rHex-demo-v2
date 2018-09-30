1192db1b5d90300b4d3f8da21174a46a84c27936
donberke <berke.official@gmail.com>
2018-09-30 21:16:12 +0200

    PWM generation enable and diable fuctions
    functions for starting and stopping pwm generations for all channels

--- rhex_base/Inc/motor_control.h
+++ rhex_base/Inc/motor_control.h
@@ -2,6 +2,11 @@
 #ifndef MOTOR_CONTROL_H
 #define MOTOR_CONTROL_H
 
+extern TIM_HandleTypeDef htim3;
+extern TIM_HandleTypeDef htim4;
+
+void PWM_start_all(void);
+void PWM_stop_all(void);
 
 void PWM_set_pulse(uint8_t pwm_pin, float pulse);
 
--- rhex_base/Src/motor_control.c
+++ rhex_base/Src/motor_control.c
@@ -3,6 +3,7 @@
 #include "motor_control.h"
 
 
+
 /*
  * Brief: Sets pulse with of a specified PWM channel
  * @pwm_pin: the number of pin
@@ -42,3 +43,34 @@ void PWM_set_pulse(uint8_t pwm_pin, float pulse) {
 		}
 	}
 }
+
+/*
+ * Brief: Enables PWM generation for all channels
+ */
+void PWM_start_all(void) {
+	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
+	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_2);
+	HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
+
+	HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_1);
+	HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_2);
+	HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_3);
+}
+
+/*
+ * Brief: Disables PWM generation for all channels
+ */
+void PWM_stop_all(void) {
+	HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_1);
+	HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_2);
+	HAL_TIM_PWM_Stop(&htim3, TIM_CHANNEL_3);
+
+	HAL_TIM_PWM_Stop(&htim4, TIM_CHANNEL_1);
+	HAL_TIM_PWM_Stop(&htim4, TIM_CHANNEL_2);
+	HAL_TIM_PWM_Stop(&htim4, TIM_CHANNEL_3);
+}
+
+
+
+
+
